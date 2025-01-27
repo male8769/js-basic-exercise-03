7f447adaab136c47b883172cbb14af4028bbe50d
male8769 <malene_storgaard@live.dk>
2025-01-27 09:37:25 +0100

    yes

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+alert(`Welcome to this JavaScript exercise!`)
+
+
+
+// 1. Link to the external JavaScript file placed in the `js` folder:
+//    ```html
+//    <script src="js/script.js"></script>
+//    ```
+// 2. Ensure "use strict" is at the top of your `script.js` file.
+
+// 3. Write a script that displays an `alert` with the following message:
+//    ```html
+//    "Welcome to this JavaScript exercise!"
+//    ```
